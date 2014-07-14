00aa29b4a761b8739e99dc4ccaabd9a74d1b9cc0
Allen Chang <teyu0523@gmail.com>
2014-07-13 23:33:29 -0700

    Not working yet

--- cordic/nbproject/Makefile-variables.mk
+++ cordic/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=cordic
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/cordic
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/cordic
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=cordic.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/cordic.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/cordic.tar
 # Release configuration
-CND_PLATFORM_Release=None-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=cordic
-CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/cordic
-CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/cordic
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=cordic.tar
-CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/cordic.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/cordic.tar
 #
 # include compiler specific variables
 #
