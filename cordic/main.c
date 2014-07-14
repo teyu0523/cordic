4cab4c969bc29ecf3c400dd459fce8f3fa0f9071
Allen Chang <teyu0523@gmail.com>
2014-07-13 20:47:30 -0700

    first commit

---
+++ cordic/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Allen
+ *
+ * Created on July 13, 2014, 8:46 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
