1e0cc3afe09a0437a712fc8501299f8ddb4687f8
mikkosan <mikkosanchez@gmail.com>
2014-07-28 21:54:18 -0700

    Updated lookup table and moved it to me local

--- optimized_main.c
+++ optimized_main.c
@@ -11,24 +11,24 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SCALE_CONST 4194304.0
+#define SCALE_CONST 33554432.0000
 #define A_CONST 1.646760
 
-int ARCTAN[29] = {
-    188743680, 111421901, 58872272, 29884485, 15000234, 7507429, 3754631, 
-    1877430, 938729, 469366, 234683, 117342, 58671, 29335, 14668, 7334, 3667,
-    1833, 917, 458, 229, 115, 57, 29, 14, 7, 4, 2, 1
-};
 
 int vectoring_mode(int x, int y, int *z){
     // the more iterative process the more percise it is
 
+    int ARCTAN[] = {
+    1509949442, 891375204, 470978178, 239075877, 120001869, 60059434, 30037045, 15019439,
+    7509834, 3754931, 1877468, 938734, 469367, 234684, 117342, 58671, 29335, 14668, 7334,
+    3667, 1833, 917, 458, 229, 115, 57, 29, 14, 7, 4, 2, 1
+    };
     register int local_z = *z;
 
     int x_new;
     int y_new;
     int i;
-    for(i=0; i<29; i++){
+    for(i=0; i<32; i++){
         if(y >= 0){
             x_new = x + (y >> i); 
             y_new = y - (x >> i); 
@@ -48,13 +48,18 @@ int vectoring_mode(int x, int y, int *z){
 int rotational_mode(int *x, int *y, int z){
     // the more iterative process the more percise it is
 
+    int ARCTAN[] = {
+    1509949442, 891375204, 470978178, 239075877, 120001869, 60059434, 30037045, 15019439,
+    7509834, 3754931, 1877468, 938734, 469367, 234684, 117342, 58671, 29335, 14668, 7334,
+    3667, 1833, 917, 458, 229, 115, 57, 29, 14, 7, 4, 2, 1
+    };
     register int local_x = *x;
     register int local_y = *y;
 
     int x_new;
     int y_new;
     int i;
-    for(i=0; i<29; i++){
+    for(i=0; i<32; i++){
         if(z < 0){
             x_new = local_x + (local_y >> i);
             y_new = local_y - (local_x >> i);
@@ -70,9 +75,10 @@ int rotational_mode(int *x, int *y, int z){
 
     *x = local_x;
     *y = local_y;
-}   
+}
 
 int main(int argc, char** argv) {
+
     int z = 0;
     int x = 1*SCALE_CONST;
     int y = 1*SCALE_CONST;
@@ -84,7 +90,7 @@ int main(int argc, char** argv) {
     // (1, 0, z)
     x = 1*SCALE_CONST;
     y = 0*SCALE_CONST;
-    z = ARCTAN[0];
+    z = 1509949442;
     rotational_mode(&x, &y, z);
     printf("sine: %lf, cosine: %lf\n", x/SCALE_CONST/A_CONST, y/SCALE_CONST/A_CONST);
     return (EXIT_SUCCESS);
